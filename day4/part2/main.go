8083f5fea91ca01300215d1b8e99dfa855097986
Sardor Bayramov <BayramovSardor@gmail.com>
2022-12-04 15:23:44 +0500

    day4

---
+++ day4/part2/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	counter := 0
+
+	for scanner.Scan() {
+		if isFullyOverlap(split(scanner.Text())) {
+			counter++
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(counter)
+}
+
+func split(str string) []int {
+	r := make([]int, 4)
+
+	x := strings.Split(str, ",")
+	p1, p2 := x[0], x[1]
+
+	x = strings.Split(p1, "-")
+	r[0], _ = strconv.Atoi(x[0])
+	r[1], _ = strconv.Atoi(x[1])
+
+	x = strings.Split(p2, "-")
+	r[2], _ = strconv.Atoi(x[0])
+	r[3], _ = strconv.Atoi(x[1])
+
+	return r
+}
+
+func isFullyOverlap(x []int) bool {
+	return !(x[1] < x[2] || x[0] > x[3])
+}
