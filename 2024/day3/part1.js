3fee1e54d02fa4eba5d992c6a1364bd1ef0656f6
bayramooov <BayramovSardor@gmail.com>
2024-12-04 21:49:01 +0500

    day3

---
+++ 2024/day3/part1.js
@@ -0,0 +1,21 @@
+const { readFile, writeFileSync } = require('fs');
+let outputText = '';
+let cout = text => (outputText += text);
+readFile('INPUT.TXT', 'utf8', (err, data) => {
+  if (err) return console.error(err);
+  const input = data;
+  // .trim()
+  // .split(/\r?\n/)
+  // .map(line =>
+  //   line
+  //     .trim()
+  //     .split(/\s+/)
+  //     .map(x => (isNaN(+x) ? String(x) : +x))
+  // );
+  main(input);
+  writeFileSync('OUTPUT.TXT', String(outputText));
+});
+
+function main(input) {
+  cout([...input.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/gm)].reduce((sum, matched) => (sum += +matched[1] * +matched[2]), 0));
+}
